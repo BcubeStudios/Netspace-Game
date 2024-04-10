9de42375d41d03a60871b23aa94eb4a076b10dbb
RafaCoronado <c6h12o6909@gmail.com>
2024-04-10 09:11:34 +0200

    base stuff

---
+++ GodotWorkspace/scripts/Point.gd
@@ -0,0 +1,15 @@
+extends Node2D
+class_name Point
+
+var pointName: String
+var coords: Vector2i
+var edges: Array[Node2D]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
