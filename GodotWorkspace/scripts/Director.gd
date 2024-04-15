22db1ea748cf3c5544fc7617f0acd1a2052be125
Abigail Verholen <abigail.verholen@etu.unistra.fr>
2024-04-15 21:33:37 +0200

    added clickable nodes with edge functions

--- GodotWorkspace/scripts/Director.gd
+++ GodotWorkspace/scripts/Director.gd
@@ -2,29 +2,46 @@ extends Control
 
 var points:Array[Point]
 var edges:Array[Edge]
+var current_point:Point
+var current_phedge:Phedge
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var point1 = $Point1
 	var point2 = $Point2
-	
+		
 	point1.activate("sillyguy", point1.global_position)
 	point2.activate("fundude", point2.global_position)
 	
 	points.append(point1)
 	points.append(point2)
+	
+	current_phedge = $phedge
+	current_phedge.reset_origin()
 	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	
-	get_viewport().get_mouse_position()
 	pass
 
 func _input(event):
-	if event is InputEventMouseButton and event.pressed:
-		# check if its inside the shape
-		for point in points:
-			if point.isInside()
+	if event is InputEventMouseButton and event.pressed and !current_phedge.is_active() and current_point != null:
+		#initalise phedge
+		current_phedge.set_origin(current_point)
+		pass
+	else:
+		if event is InputEventMouseButton and event.pressed and current_phedge.is_active() and current_point != null:
+			#new edge
+			var edge = Edge.new(current_phedge.origin, current_point)
+			print("edge made")
+			#reset phedge 
+			current_phedge.reset_origin()
+			#add edge to array
+			edges.append(edge)
+			pass
 	pass
+	
+
+func change_current_point(point):
+	current_point = point
--- GodotWorkspace/scripts/Point.gd
+++ GodotWorkspace/scripts/Point.gd
@@ -1,6 +1,8 @@
 extends Node2D
 class_name Point
 
+@onready var director = $"../"
+
 var pointName: String
 var coords: Vector2i
 var edges: Array[Edge]
@@ -10,10 +12,10 @@ func _ready():
 	self.hide()
 	pass # Replace with function body.
 
-func activate(name, coords):
+func activate(point_name, point_coords):
 	self.show()
-	self.pointName = name
-	self.coords = coords
+	self.pointName = point_name
+	self.coords = point_coords
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,9 +30,16 @@ func do_left_click():
 	print("left " + pointName)
 	pass
 	
-func is_inside(position):
-	pass
-
 
 func _draw(): 
 	pass
+
+
+func _on_area_2d_mouse_entered():
+	director.change_current_point(self)
+	print("Point changed!")
+	print(pointName)
+
+func _on_area_2d_mouse_exited():
+	director.change_current_point(null)
+	print("No longer pointing!")
