15acd009d684a2429009363774171e4ec5ede6a0
RafaCoronado <c6h12o6909@gmail.com>
2024-04-16 12:00:30 +0200

    some ordering

--- GodotWorkspace/scripts/Director.gd
+++ GodotWorkspace/scripts/Director.gd
@@ -2,17 +2,20 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var point1 = Point.new("point1", Vector2i(150, 150))
-	var point2 = Point.new("point2", Vector2i(300, 150))
-	add_child(point1) 
-	add_child(point2)
-	CreateEdge(point1, point2)
+	var point1 = createPoint("point1", Vector2i(150, 150))
+	var point2 = createPoint("point2", Vector2i(300, 150))
+	var edge = createEdge(point1, point2)
 	pass # Replace with function body.
 
-func CreateEdge(point1, point2):
+func createEdge(point1, point2) -> Edge:
 	var newEdge = Edge.new(point1, point2)
 	add_child(newEdge)
+	return newEdge
 
+func createPoint(name, coords) -> Point:
+	var newPoint = Point.new(name, coords)
+	add_child(newPoint)
+	return newPoint
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
