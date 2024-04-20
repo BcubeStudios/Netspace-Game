c4a4ccd2e795c44d949156910f3a14abf8c1642b
Abigail Verholen <abigail.verholen@etu.unistra.fr>
2024-04-20 16:33:08 +0200

    added max length total

--- GodotWorkspace/scripts/Edge.gd
+++ GodotWorkspace/scripts/Edge.gd
@@ -5,6 +5,7 @@ var point1: Point
 var point2: Point
 var colour: Color = Global.Colours["black"]
 var id: String
+var length: float
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,6 +18,7 @@ func _init(new_point1, new_point2):
 	self.id = str(max(point1.coords.x, point2.coords.x))+ \
 	str(min(point1.coords.x, point2.coords.x)) + str(max(point1.coords.y, point2.coords.y)) +\
 	str(min(point1.coords.y, point2.coords.y))
+	self.length = sqrt(((point1.coords.x - point2.coords.x)**2) * ((point1.coords.y - point2.coords.y)**2))
 
 func color_change():
 	pass
