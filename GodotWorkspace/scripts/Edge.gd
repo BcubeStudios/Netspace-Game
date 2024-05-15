3aa0edefb9a0d52e2add47ce9f369c24fe1f58ef
RafaCoronado <c6h12o6909@gmail.com>
2024-05-15 14:57:28 +0200

    changing the main code
    changing code on the director to accommodate  edge instantiating in order to allow mouse detection

--- GodotWorkspace/scripts/Edge.gd
+++ GodotWorkspace/scripts/Edge.gd
@@ -1,6 +1,9 @@
 extends Node2D
 class_name Edge
 
+@onready var director = $"../"
+@onready var collision_shape_2d = $Area2D/CollisionShape2D
+
 var point1: AbstractPoint
 var point2: AbstractPoint
 var colour: Color = Global.Colours["black"]
@@ -8,17 +11,25 @@ var id: String
 var length: float = 0
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_area_2d_mouse_entered():
+	director.change_pointing_at(self)
+	print("enter")
 
-func _init(new_point1, new_point2):
+func _on_area_2d_mouse_exited():
+	director.change_pointing_at(null)
+	print("exit")
+
+func activate(new_point1, new_point2):
 	self.point1 = new_point1
 	self.point2 = new_point2
+	
 	self.id = str(max(point1.coords.x, point2.coords.x))+ \
 	str(min(point1.coords.x, point2.coords.x)) + str(max(point1.coords.y, point2.coords.y)) +\
 	str(min(point1.coords.y, point2.coords.y))
 	self.length = sqrt(((point1.coords.x - point2.coords.x)**2) + ((point1.coords.y - point2.coords.y)**2))
+	
+	#collision_shape_2d.shape.a = new_point1.coords
+	#collision_shape_2d.shape.a = new_point1.coords
 	
 
 func color_change():
--- GodotWorkspace/scripts/director.gd
+++ GodotWorkspace/scripts/director.gd
@@ -2,10 +2,12 @@ extends Control
 
 var point_scene = preload("res://scenes/point.tscn")
 var end_point_scene = preload("res://scenes/end_point.tscn")
+var edge_scene = preload("res://scenes/edge.tscn")
 
 var points:Array[AbstractPoint]
 var edges:Array[Edge]
 var current_point:AbstractPoint
+var current_edge:Edge
 var current_phedge:Phedge
 var length_left: float
 var curr_length: float
@@ -41,51 +43,75 @@ func _process(_delta):
 	
 
 func _input(event):
-	if event is InputEventMouseButton and event.pressed and current_point == null:
-		#reset phedge 
+	if event is InputEventMouseButton and event.pressed and current_point == null and current_phedge.is_active():
+		# reset phedge 
 		current_phedge.reset_origin()
 		curr_length = 0
-	else:
-		if event is InputEventMouseButton and event.pressed and !current_phedge.is_active() and current_point != null:
-			#initalise phedge
-			if current_point is EndPoint and current_point.edge != null:
-				return
-			current_phedge.set_origin(current_point, min(max_phedge, length_left))
+	elif event is InputEventMouseButton and event.pressed and !current_phedge.is_active() and current_point != null:
+		# initalise phedge
+		if current_point is EndPoint and current_point.edge != null:
+			return
+		current_phedge.set_origin(current_point, min(max_phedge, length_left))
+	elif event is InputEventMouseButton and event.pressed and current_phedge.is_active():
+		if current_point != null and current_phedge.is_valid(current_point.coords):
+			# try to create edge
+			create_edge(current_phedge.origin, current_point)
 		else:
-			if event is InputEventMouseButton and event.pressed and current_phedge.is_active():
-				if current_point != null and current_phedge.is_valid(current_point.coords):
-					#new edge
-					var edge = Edge.new(current_phedge.origin, current_point)
-					if(current_phedge.origin.add_edge(edge) and current_point.add_edge(edge) and change_length_left(edge.length)):
-						#reset phedge 
-						current_phedge.reset_origin()
-						#add edge to array
-						add_child(edge)
-						#move to almost top
-						move_child(edge, 1)
-						edges.append(edge)
-						game_won()
-					else:
-						#reset phedge 
-						current_phedge.reset_origin()
-						curr_length = 0
-				else:
-					current_phedge.reset_origin()
+			current_phedge.reset_origin()
+	elif event is InputEventMouseButton and event.pressed and current_edge != null:
+		delete_edge(current_edge)
 
 
-func change_current_point(point):
-	self.current_point = point
-	
+func change_pointing_at(node):
+	if node is AbstractPoint:
+		self.current_point = node
+	elif node is Edge:
+		self.current_edge = node
+	else:
+		self.current_edge = node
+		self.current_point = node
+
+func create_edge(point1, point2):
+	var edge = edge_scene.instantiate()
+	edge.activate(point1, point2)
+	# check if the edge would be valid
+	if(current_phedge.origin.add_edge(edge) and current_point.add_edge(edge) and change_length_left(edge.length)):
+		# reset phedge 
+		current_phedge.reset_origin()
+		# add edge to array
+		add_child(edge)
+		# move to almost top
+		move_child(edge, 1)
+		edges.append(edge)
+		game_won()
+	# abort edge creation if not valid
+	else:
+		# reset phedge 
+		current_phedge.reset_origin()
+		curr_length = 0
+
+func delete_edge(edge):
+	# unlink edge from both it's points
+	edge.point1.remove_edge(edge)
+	edge.point2.remove_edge(edge)
+	# refund used length
+	change_length_left(-edge.length)
+	# remove edge from the director
+	remove_child(edge)
+	edges.erase(edge)
+	edge.queue_free()
+
+
 func change_length_left(length: float) -> bool:
 	var new_length = length_left - length
 	if(new_length >= 0):
 		length_left = new_length
 		return true
 	return false
-	
-func game_won() -> bool:	
+
+func game_won() -> bool:
 	if edges.is_empty():
-		return false	
+		return false
 	
 	var connected_points:Array[AbstractPoint] = []
 	connected_points.append(edges[0].point1)
@@ -105,7 +131,7 @@ func game_won() -> bool:
 					connected_points.append(edge.point2)
 	
 	if connected_points.size() == points.size():
-		#weve won so we can call the level to win 
+		# we've won so we can call the level to win 
 		$"../../".level_won()
 		return true
 	return false
@@ -114,28 +140,29 @@ func auto_solve():
 	var solved_points:Array[AbstractPoint] = []
 	var remaining_points:Array[AbstractPoint] = points.duplicate()
 	
-	#first point random
+	# first point random
 	points.shuffle()
 	solved_points.append(points[0])
 	
-	#remove from remaining
+	# remove from remaining
 	remaining_points.erase(solved_points[0])
 	
 	var min_edge:Edge
 	
 	while(not self.game_won()):
-		#find minimum edge
+		# find minimum edge
 		min_edge = null
 		for point in solved_points:
 			for new_point in remaining_points:
-				var edge = Edge.new(point, new_point)
+				var edge = edge_scene.instantiate()
+				edge.activate(point, new_point)
 				if min_edge == null:
 					min_edge = edge
 				if edge.length <= max_phedge and edge.length < min_edge.length  and change_length_left(edge.length)\
 				 and point.possible_edge(edge) and new_point.possible_edge(edge):
 					min_edge = edge
 					change_length_left(-(edge.length))
-		#add edge
+		# add edge
 		if min_edge != null and min_edge.length <= max_phedge and change_length_left(min_edge.length) \
 		and min_edge.point1.add_edge(min_edge) and min_edge.point2.add_edge(min_edge):
 			edges.append(min_edge)
@@ -144,8 +171,9 @@ func auto_solve():
 			remaining_points.erase(min_edge.point2)
 			
 			add_child(min_edge)
-			#move to almost top
+			# move to almost top
 			move_child(min_edge, 1)
-			#draw edge
+			# draw edge
 			min_edge.queue_redraw()
 			await Global.wait(0.5)
+
