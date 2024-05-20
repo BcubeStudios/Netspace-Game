339b2b1a2cd28fdd94c5e0ca0ee012110a000bd6
minihic <minihic@users.noreply.github.com>
2024-05-20 20:52:56 +0000

    Deploying to gh-pages from @ BcubeStudios/SE-2024-Code@83a71a61ae71d0bb40d40d2ed2c5930ccba93c93 🚀

--- GodotWorkspace/scripts/global.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-var Colours = {"red" = Color(0.671, 0, 0), "blue" = Color(0.078, 0.122, 0.988),  "green" = Color(0, 1, 0.094), "black" = Color(0, 0, 0)}
-
-var First_time:bool = true
-
-func wait(duration):  
-	await get_tree().create_timer(duration, false, false, true).timeout
