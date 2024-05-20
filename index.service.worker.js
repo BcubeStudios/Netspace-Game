0c303e69bf253e806b9d81b0272ccd4c7ffe2695
minihic <minihic@users.noreply.github.com>
2024-05-20 21:26:02 +0000

    Deploying to gh-pages from @ BcubeStudios/SE-2024-Code@33aa8985d813c17ca79a82b41f4e99c4215951c3 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1716240095|4714421";
+const CACHE_VERSION = "1716240355|5115583";
 const CACHE_PREFIX = "Se-2024-code-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
